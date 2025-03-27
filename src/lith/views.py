99a6bb0e46ccf94887eccd2038439459f88e0b7d
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-03-27 20:11:48 +0800

    Add PEP8 violation

--- src/lith/views.py
+++ src/lith/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, JsonResponse, HttpResponse
 
 
-def home(request: HttpRequest) -> HttpResponse:
+def home(request: HttpRequest) -> HttpResponse: # This should trigger a PEP8 violation for being too long of a line.
     return JsonResponse({"message": "hello, world"})
