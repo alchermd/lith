7659dad7988462945c929d5e90d192d9c144c04b
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-03-28 17:28:41 +0800

    Add postgresql

--- src/config/settings.py
+++ src/config/settings.py
@@ -60,9 +60,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": os.environ["DB_ENGINE"],
+        "NAME": os.environ["DB_DATABASE"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
+    },
 }
 
 # Password validation
