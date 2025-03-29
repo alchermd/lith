15b3d044a6dc740b2ca4ba01c8d2ee7373884ef3
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-03-29 21:06:05 +0800

    Add case insensitive email uniqueness constraint

--- src/lith/models/auth.py
+++ src/lith/models/auth.py
@@ -1,10 +1,14 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import (
     AbstractBaseUser,
     UserManager as DjangoUserManager,
     PermissionsMixin,
 )
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models.functions import Lower
 from django.utils.translation import gettext as _
 
 
@@ -52,3 +56,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                Lower("email"), name="unique_email", violation_error_message="Email already exists."
+            )
+        ]
