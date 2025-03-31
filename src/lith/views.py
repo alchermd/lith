6bac9edb8421d57bfa3f1456d3ad1e9e75be6628
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-04-01 00:09:40 +0800

    Create a form class for validation

--- src/lith/views.py
+++ src/lith/views.py
@@ -3,19 +3,29 @@ from django.contrib.auth import authenticate
 from django.http import HttpRequest, JsonResponse, HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 
+from .forms.auth import LoginForm
+
 
 def home(request: HttpRequest) -> HttpResponse:
     return JsonResponse({"message": "hello, world"})
 
 
-def login(request: HttpRequest) -> HttpResponseRedirect | None:
-    email = request.POST.get("email")
-    password = request.POST.get("password")
+def login(request: HttpRequest) -> HttpResponseRedirect | HttpRequest:
+    status = 200
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
 
-    user = authenticate(username=email, password=password)
+            user = authenticate(username=email, password=password)
 
-    if user is None:
-        messages.error(request, "Invalid credentials")
-        return render(request, "lith/auth/login.html", status=401)
+            if user is not None:
+                return redirect("/dashboard")
 
-    return redirect("/dashboard")
+            messages.error(request, "Invalid credentials")
+            status = 401
+    else:
+        form = LoginForm()
+
+    return render(request, "lith/auth/login.html", status=status)
