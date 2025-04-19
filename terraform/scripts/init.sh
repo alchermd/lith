37e1cebfe48156a28576c733b90f0763d4643549
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-04-19 18:02:54 +0800

    Use TF_ROOT_DIR instead of ROOT_DIR

--- terraform/scripts/init.sh
+++ terraform/scripts/init.sh
@@ -6,7 +6,7 @@ ROOT_DIR=$(pwd)
 TF_ROOT_DIR="${ROOT_DIR}/terraform"
 BOOTSTRAP_DIR="${TF_ROOT_DIR}/bootstrap"
 
-for d in $BOOTSTRAP_DIR $ROOT_DIR; do
+for d in $BOOTSTRAP_DIR $TF_ROOT_DIR; do
   cd "$d"
   terraform init
 done
